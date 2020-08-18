7b28b5012aeeb4346bbe8f8c25f34f21539691b2
Patrick Uhlmann <git@uhlme.ch>
2020-08-18 23:29:37 +0200

    Add Bulma for Styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button class="button is-primary">This is a button</button>
   </div>
 </template>
 
