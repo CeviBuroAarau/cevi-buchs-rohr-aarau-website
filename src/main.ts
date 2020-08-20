cd37cd4de7f3ce07f8718108780eb818708689c9
Patrick Uhlmann <git@uhlme.ch>
2020-08-20 07:52:49 +0200

    Applied Lint Rules

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,19 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { dom } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { dom } from "@fortawesome/fontawesome-svg-core";
 
-dom.watch()
+dom.watch();
 
 Vue.config.productionTip = false;
 
 require("./assets/main.scss");
 
-library.add(fas)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,
