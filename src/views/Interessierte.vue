72a4b284120f9b18204540cbd930cc142ade708b
Patrick Uhlmann <git@uhlme.ch>
2020-08-22 23:26:11 +0200

    Interessierte Page

--- src/views/Interessierte.vue
+++ src/views/Interessierte.vue
@@ -1,7 +1,19 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Interessierte</h1>
+      <h1 class="title is-1">Möchten Sie uns kennenlernen?</h1>
+      <p class="content">
+        Gerne können Kinder kostenlos und unverbindlich an
+        <router-link to="/mitglieder/agenda"
+          >jedem regulären Nachmittag</router-link
+        >
+        reinschnuppern.
+        <router-link to="/interessierte/schnuppern"
+          >Anmeldung und weitere Informationen</router-link
+        >. In diesem Bereich finden Sie auch Informationen zu unseren
+        <router-link to="/interessierte/aktivitaeten">Aktivitäten</router-link
+        >, dem <router-link to="/interessierte/cevi_schweiz">Cevi Schweiz</router-link> oder <router-link to="/interessierte/standort">wo Sie uns finden</router-link>
+      </p>
     </div>
   </section>
 </template>
