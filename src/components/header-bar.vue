9efd98ecfcfc8a7bedf41cfee4ee94fd073fb7a5
Patrick Uhlmann <git@uhlme.ch>
2020-08-24 13:54:04 +0200

    Bugfix logo link should point to mainpage

--- src/components/header-bar.vue
+++ src/components/header-bar.vue
@@ -13,7 +13,7 @@ export default {
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+      <a class="navbar-item" href="/">
         <img src="@/assets/logo.svg" />
       </a>
 
