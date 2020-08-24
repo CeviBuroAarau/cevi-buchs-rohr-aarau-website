74f6b256857d6516713f577a2da7483944100f2e
Patrick Uhlmann <git@uhlme.ch>
2020-08-24 13:49:35 +0200

    Cevi Schweiz Page

--- src/views/CeviSchweiz.vue
+++ src/views/CeviSchweiz.vue
@@ -1,7 +1,20 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Cevi Schweiz</h1>
+      <h1 class="title is-1">Cevi Schweiz</h1>
+      <h2 class="title is-2">Was ist der Cevi?</h2>
+      <p class="content">
+      Der <a href="https://www.cevi.ch/portrait/">Cevi</a> ist der drittgrösste Jugendverband in der Schweiz. Er umfasst rund 13 000 Mitglieder in über 200 lokalen Vereinen, führt jährlich über 80 Ausbildungskurse und gegen 300 Lager durch. Als christlich und international ausgerichtete Jugendbewegung stehen Leiterschaft und das Befähigen von Menschen im Zentrum aller Tätigkeiten. Der Cevi Schweiz ist Teil der europäischen und weltweiten Cevi-Verbände <a href="https://www.ymca.int/">YMCA</a> und <a href="https://www.worldywca.org/">YWCA</a> mit insgesamt 70 Millionen Mitgliedern.
+      </p>
+      <h2 class="title is-2">Was ist das Ziel der Cevi?</h2>
+      <p class="content">
+        Der Cevi ist in der schnelllebigen Konsumgesellschaft ein wichtiger Kontrapunkt. Die Kinder und Jugendlichen lernen, ihre Talente zu entwickeln, sowie mit sich und der Umwelt sorgsam umzugehen. Sie werden dabei unterstützt, ihr Leben sinnvoll zu gestalten und in der Gesellschaft Verantwortung zu übernehmen.
+      </p>
+      <p class="content">
+        Der Cevi leistet durch seine Tätigkeit einen Beitrag zu einer offenen, aber dennoch verbindlichen Gesellschaft, in welcher Menschen in ihrer Persönlichkeit gestärkt werden, sich anhand ihrer Stärken einbringen und Verantwortung übernehmen können. Ein wichtiger Grundsatz ist es alle Meinungen und Wertvorstellungen zu akzeptieren und zu respektiveren.
+      </p>
+      <h2 class="title is-2">Wie sind wir als lokaler Verein integriert?</h2>
+      <p class="content">Der Cevi ist in 7 Regionalverbände unterteilt. Wir sind Mitglied des Verbands <a href="https://cevi-agsoluzg.ch/portrait/">AG-SO-LU-ZG</a> (Abkürzung der Kantone). Dieser besteht aus 30 Vereinen und ca. 1'500 Mitgliedern. Er ist unsere Verbindung zum Dachverband. Wir können darüber unsere Anliegen einbringen und erhalten Informationen. Zudem organisiert der Regionalverband <a href="https://www.jugendundsport.ch/">J+S</a> (Jugend und Sport) Ausbildungskurse, welche vom Bundesamt für Sport anerkannt sind und unsere Leiter befähigen Gruppen zu führen. </p>
     </div>
   </section>
 </template>
