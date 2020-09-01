4c6d3dfea5cb52ba63116ca08ee0d7ac9d905482
Patrick Uhlmann <git@uhlme.ch>
2020-09-01 22:46:07 +0200

    Standort Page
    Using Leaflet

--- src/views/Standort.vue
+++ src/views/Standort.vue
@@ -2,6 +2,68 @@
   <section class="section">
     <div class="container">
       <h1 class="title">Standort</h1>
+      <p class="content">
+        Wir werden von der 
+        <a href="https://www.ref-kirche-buchs-rohr.ch/">reformierten Kirchgemeinde Buchs-Rohr</a> 
+        unterstützt. Daher können wir deren Räumlichkeiten benützen. 
+        In der Regel starten und enden unsere Nachmittage im Pfarrhaus 
+        Ausserdorf. Ausnahmsweise kann sich der Besammlungsort 
+        ändern. Dies wird jeweils in der 
+        <router-link to="/mitglieder/agenda">Agenda</router-link> kommuniziert.
+      </p>
+      <h2 class="title is-2">Pfarrhaus Ausserdorf</h2>
+
+  <div style="height: 500px; width: 100%">
+    <l-map
+      :zoom="zoom"
+      :center="center"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-marker :lat-lng="pfarrhaus">
+        <l-tooltip>
+          <p>Besammlungsort</p>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+  </div>
+
+      <p class="content">
+      Das Pfarrhaus ist direkt zwischen dem Alterszentrum Suhrhard 
+      und der Brücke über die Aaretalstrasse. Die nächste 
+      Bushaltestelle Alterszentrum ist nur ca. 150 m entfernt.
+      </p>
+      <p class="content">
+        Das Lokal sieht wie folgt aus:<br />
+        <img src="@/assets/standort/lokal.jpg" width="800" />
+      </p>
     </div>
   </section>
 </template>
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LTooltip } from "vue2-leaflet";
+import 'leaflet-defaulticon-compatibility';
+
+export default {
+  name: "Example",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 16,
+      center: latLng(47.392085, 8.084783),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      pfarrhaus: latLng(47.392460, 8.084986),
+    };
+  },
+};
+</script>
