dd1e5b38b4f7570a4162bcfb8701510d53826217
Patrick Uhlmann <git@uhlme.ch>
2020-09-05 19:34:43 +0200

    Add 404 Error page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Seite existiert nicht</h1>
+      <p class="content">
+        Die aufgerufene Seite existiert leider nicht. Möglicherweise ist die Information unterdessen auf einer anderen Seite verfügbar. Vielleicht findest du Sie mit der Suchen Funktion.
+      </p>
+    </div>
+  </section>
+</template>
+<script>
+export default {};
+</script>
