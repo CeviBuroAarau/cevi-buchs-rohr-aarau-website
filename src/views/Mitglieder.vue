0c887fe005c917d07ec1cf394924b14c742eff6d
Patrick Uhlmann <git@uhlme.ch>
2020-09-05 19:48:05 +0200

    Lint Fix

--- src/views/Mitglieder.vue
+++ src/views/Mitglieder.vue
@@ -3,17 +3,19 @@
     <div class="container">
       <h1 class="title">Mitglieder</h1>
       <p class="content">
-        Unser Mitgliederbereich bietet Dir in der 
+        Unser Mitgliederbereich bietet Dir in der
         <router-link to="/mitglieder/agenda">Agenda</router-link>
-        Informationen zu aktuellen Anlässen. Im 
+        Informationen zu aktuellen Anlässen. Im
         <router-link to="/mitglieder/album">Album</router-link>
-        findest du Impressionen vergangener Anlässe. Wenn Du dich 
-        ausrüsten möchtest gibt es verschiedene Artikel in 
-        unserem 
-        <router-link to="/mitglieder/laedeli">Cevi-Lädeli</router-link>. 
-        Gerne zeigen wir Dir auch die 
-        <router-link to="/mitglieder/ausbildung">Ausbildungsmöglichkeiten</router-link>
-        und stellen das <router-link to="/mitglieder/leiterteam">Leiterteam</router-link> 
+        findest du Impressionen vergangener Anlässe. Wenn Du dich ausrüsten
+        möchtest gibt es verschiedene Artikel in unserem
+        <router-link to="/mitglieder/laedeli">Cevi-Lädeli</router-link>. Gerne
+        zeigen wir Dir auch die
+        <router-link to="/mitglieder/ausbildung"
+          >Ausbildungsmöglichkeiten</router-link
+        >
+        und stellen das
+        <router-link to="/mitglieder/leiterteam">Leiterteam</router-link>
         vor.
       </p>
     </div>
