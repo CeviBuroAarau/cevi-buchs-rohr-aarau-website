c44a8f3e4e295a7093e1a6d3b8fb7634e8155964
Patrick Uhlmann <git@uhlme.ch>
2020-10-03 23:04:33 +0200

    Cevi Layout implemented
    There are two layouts:
    Front.vue for the main page. It features a full-screen image and special menu
    Regular for all other pages. It features a regular menu on the left side

--- src/components/header-bar.vue
+++ src/components/header-bar.vue
@@ -3,86 +3,48 @@ export default {
   name: "HeaderBar",
   components: {},
   data() {
-    return {
-      showNav: false
-    };
+    return {};
   }
 };
 </script>
 
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="/">
-        <img src="@/assets/logo.svg" />
-      </a>
-
-      <a
-        role="button"
-        @click="showNav = !showNav"
-        :class="{ 'is-active': showNav }"
-        class="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbar"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbar" class="navbar-menu" :class="{ 'is-active': showNav }">
-      <div class="navbar-start">
-        <router-link class="navbar-item" to="/">Home</router-link>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <router-link class="navbar-link" to="/interessierte"
-            >Interessierte</router-link
-          >
-
-          <div class="navbar-dropdown">
-            <router-link class="navbar-item" to="/interessierte/schnuppern"
-              >Schnuppern</router-link
-            >
-            <router-link class="navbar-item" to="/interessierte/aktivitaeten"
-              >Aktivitäten</router-link
-            >
-            <router-link class="navbar-item" to="/interessierte/cevi_schweiz"
-              >Cevi Schweiz</router-link
-            >
-            <router-link class="navbar-item" to="/interessierte/standort"
-              >Standort</router-link
-            >
-          </div>
-        </div>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <router-link class="navbar-link" to="/mitglieder"
-            >Mitglieder</router-link
-          >
-
-          <div class="navbar-dropdown">
-            <router-link class="navbar-item" to="/mitglieder/agenda"
-              >Agenda</router-link
-            >
-            <router-link class="navbar-item" to="/mitglieder/album"
-              >Album</router-link
-            >
-            <router-link class="navbar-item" to="/mitglieder/laedeli"
-              >Lädeli</router-link
-            >
-            <router-link class="navbar-item" to="/mitglieder/ausbildung"
-              >Ausbildung</router-link
-            >
-            <router-link class="navbar-item" to="/mitglieder/leiterteam"
-              >Leiterteam</router-link
-            >
-          </div>
-        </div>
-
-        <router-link class="navbar-item" to="/kontakt">Kontakt</router-link>
-      </div>
-    </div>
-  </nav>
+  <div class="header">
+    <div class="mobile-menu"><font-awesome-icon icon="bars" /></div>
+    <router-link to="/"
+      ><img class="logo" src="@/assets/logo.svg"
+    /></router-link>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.header {
+  background: white;
+  height: 50px;
+  border-bottom: 1px solid #000;
+}
+
+.header .mobile-menu {
+  display: none;
+}
+
+.header .logo {
+  height: 40px;
+  margin: 5px 8px;
+}
+
+/* mobile */
+@media only screen and (max-width: 768px) {
+  .header {
+    text-align: right;
+  }
+
+  .header .mobile-menu {
+    display: block;
+    position: absolute;
+    left: 10px;
+    font-size: 42px;
+    line-height: 50px;
+  }
+}
+</style>
