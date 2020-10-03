deaefd4667234360ef7e63259fb46c87db956389
Patrick Uhlmann <git@uhlme.ch>
2020-10-03 23:04:47 +0200

    express script linted

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');app = express();
-app.use(serveStatic(__dirname + "/dist"));var port = process.env.PORT || 5000;
-app.listen(port);console.log('server started '+ port);
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log("server started " + port);
