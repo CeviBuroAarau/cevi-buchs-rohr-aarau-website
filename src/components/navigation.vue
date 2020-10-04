840b9ecf9444fd6a6fc678a3a3b4946822cbfbf8
Patrick Uhlmann <git@uhlme.ch>
2020-10-04 15:32:55 +0200

    lint

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -4,7 +4,8 @@ export default {
   components: {},
   data() {
     return {
-      activeBackgroundImage: (process.env.VUE_APP_EXTERNAL_ASSETS_URI + "/home/background8.jpg")
+      activeBackgroundImage:
+        process.env.VUE_APP_EXTERNAL_ASSETS_URI + "/home/background8.jpg"
     };
   },
   props: {
