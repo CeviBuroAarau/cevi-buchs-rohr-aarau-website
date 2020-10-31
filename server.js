36fd6d1933bb57132f037532eeee294a12f65854
Patrick Uhlmann <git@uhlme.ch>
2020-10-31 15:32:15 +0100

    heroku hosting non-root paths
    enable the server to react to non-root paths (e. g. /cevianer/agenda)

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const path = require("path");
+const history = require('connect-history-api-fallback')
+const path = require('path');
 const serveStatic = require("serve-static");
+
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    //verbose: true
+}))
+
+app.use(serveStatic(path.join(__dirname + "/dist")));
+
 const port = process.env.PORT || 5000;
-app.listen(port);
-console.log("server started " + port);
+
+app.listen(port, () => {
+    console.log("server started " + port);
+});
