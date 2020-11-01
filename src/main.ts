4aa1ef1d48bd03c70bccb833650b283abf1a135c
Patrick Uhlmann <git@uhlme.ch>
2020-11-01 13:47:37 +0100

    minimize size fontawesome
    only import the icons that are actually used. this saves about 650kb!

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons/faPhone";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
+
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { dom } from "@fortawesome/fontawesome-svg-core";
 
@@ -14,7 +17,7 @@ Vue.config.productionTip = false;
 
 require("./assets/main.scss");
 
-library.add(fas);
+library.add(faPhone, faEnvelope, faUser);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
