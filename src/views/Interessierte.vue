cdc1a4a159ba95e9d80a4aff08703fe0f48c6b81
Patrick Uhlmann <git@uhlme.ch>
2020-11-09 21:19:21 +0100

    Interessierte: new text

--- src/views/Interessierte.vue
+++ src/views/Interessierte.vue
@@ -2,22 +2,33 @@
   <section class="section">
     <div class="container">
       <h1 class="title is-1">Möchten Sie uns kennenlernen?</h1>
-      <p class="content">
-        Gerne können Kinder kostenlos und unverbindlich an
-        <router-link to="/cevianer/agenda"
-          >jedem regulären Nachmittag</router-link
-        >
-        reinschnuppern.
-        <router-link to="/interessierte/schnuppern"
-          >Anmeldung und weitere Informationen</router-link
-        >. In diesem Bereich finden Sie auch Informationen zu unseren
-        <router-link to="/interessierte/aktivitaeten">Aktivitäten</router-link>,
-        dem
-        <router-link to="/interessierte/cevi_schweiz">Cevi Schweiz</router-link>
-        oder
-        <router-link to="/interessierte/standort"
-          >wo Sie uns finden</router-link
-        >
+      <p class="content">Gerne</p>
+      <ul>
+        <li>
+          ... erklären wir
+          <router-link to="/interessierte/cevi_schweiz"
+            >was der Cevi ist</router-link
+          >
+        </li>
+        <li>
+          ... wo wir
+          <router-link to="/interessierte/standort">uns treffen</router-link>
+        </li>
+        <li>
+          ... was wir an einem Jungschinachmittag für
+          <router-link to="/interessierte/standort"
+            >Aktivitäten durchführen</router-link
+          >
+        </li>
+      </ul>
+
+      <p>
+        Ihre Kinder sind eingeladen kostenlos und unverbindlich an jedem
+        beliebigen
+        <router-link to="/cevianer/agenda">Jungschidatum</router-link>
+        vorbeizukommen und
+        <router-link to="/interessierte/schnuppern">reinschnuppern</router-link
+        >.
       </p>
     </div>
   </section>
