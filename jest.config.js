c4efbe353ed2708a45c9bb3b67f40f45221119fb
Patrick Uhlmann <git@uhlme.ch>
2021-02-18 20:13:48 +0100

    Limit Coverage Calculation
    to files that are effectively relevant

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   "collectCoverage": true,
-  "collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**"],
+  "collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**", "!coverage/**", "!dist/**"],
   "coverageDirectory": "coverage"
 };
