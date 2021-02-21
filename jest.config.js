906a8f23e9743a0c62cc87a6899b5e167be09e7d
Patrick Uhlmann <git@uhlme.ch>
2021-02-21 14:52:19 +0100

    lint jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  "collectCoverage": true,
-  "coverageDirectory": "coverage",
-  "collectCoverageFrom": [
-    "src/**/*.{vue,ts,js}",
-    "!**/node_modules/**"
-  ],
-  "coverageThreshold": {
-    "global": {
-      "branches": 25,
-      "functions": 25,
-      "lines": 25
-    }
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.{vue,ts,js}", "!**/node_modules/**"],
+  coverageThreshold: {
+    global: {
+      branches: 25,
+      functions: 25,
+      lines: 25,
+    },
   },
-  "testResultsProcessor": "jest-sonar-reporter",
+  testResultsProcessor: "jest-sonar-reporter",
 };
