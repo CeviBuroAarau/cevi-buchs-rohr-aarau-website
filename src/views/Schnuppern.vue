a1b73bd08b2067db8af644517e226d1d73379bba
Patrick Uhlmann <git@uhlme.ch>
2021-02-23 22:59:31 +0100

    Add Sentry Error Logging
    Replace console.log with Sentry.captureException

--- src/views/Schnuppern.vue
+++ src/views/Schnuppern.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from "axios";
+import * as Sentry from "@sentry/vue";
 
 export default {
   components: {},
@@ -48,8 +49,8 @@ export default {
           this.message = "";
         })
         .catch((err) => {
-          console.log(err);
           this.displayError = true;
+          Sentry.captureException(err);
         });
     },
   },
