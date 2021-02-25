a1ff87b2095aff7d11f0b83de4f2dfd5c51babcb
Patrick Uhlmann <git@uhlme.ch>
2021-02-25 22:25:53 +0100

    lint

--- src/views/Version.vue
+++ src/views/Version.vue
@@ -3,7 +3,8 @@
     <div class="container">
       <h1 class="title is-1">Version</h1>
       <p class="content">
-        {{ this.version }} erstellt am {{ this.date }} basierend auf der (Revision {{ this.revision }})
+        {{ this.version }} erstellt am {{ this.date }} basierend auf der
+        (Revision {{ this.revision }})
       </p>
     </div>
   </section>
@@ -13,10 +14,9 @@ export default {
   components: {},
   data() {
     return {
-      version:
-        process.env.VUE_APP_VERSION,
+      version: process.env.VUE_APP_VERSION,
       revision: process.env.VUE_APP_REVISION,
-      date: process.env.VUE_APP_DATE
+      date: process.env.VUE_APP_DATE,
     };
   },
 };
