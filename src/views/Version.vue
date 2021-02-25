2e478631ddec349265321548b2e24b67c1e2f04c
Patrick Uhlmann <git@uhlme.ch>
2021-02-25 19:57:04 +0100

    introduce a version page

---
+++ src/views/Version.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title is-1">Version</h1>
+      <p class="content">
+        Version der Seite: {{ this.version }}
+      </p>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      version:
+        process.env.VUE_APP_VERSION,
+    };
+  },
+};
+</script>
