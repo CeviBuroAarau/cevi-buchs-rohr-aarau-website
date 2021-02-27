a22a7729ec3ca384e2efb62533b14309c697fcf6
Patrick Uhlmann <git@uhlme.ch>
2021-02-27 16:20:51 +0100

    sentry sourcemap upload
    will be done only in production mode. a token with the scope
    project:write obtained under
    https://sentry.io/settings/account/api/auth-tokens/ needs to be provided
    in the .env.local file
    
    set sentry token for the workflows
    
    read them from the secret "SENTRY_TOKEN"

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,50 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 
+// eslint-disable-next-line
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
 module.exports = {
   devServer: {
     port: 8080,
   },
-  configureWebpack: {
-    plugins: [
-      new BundleAnalyzerPlugin({
-        analyzerMode: "static",
-        openAnalyzer: false,
-      }),
-    ],
-    devtool: "source-map",
+  // eslint-disable-next-line
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        plugins: [
+          new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+          }),
+          new SentryWebpackPlugin({
+            // sentry-cli configuration
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            org: process.env.SENTRY_ORG,
+            project: process.env.SENTRY_PROJECT,
+            release: process.env.VUE_APP_VERSION,
+
+            // webpack specific configuration
+            include: "./dist",
+            ignore: ["node_modules", "webpack.config.js"],
+          }),
+        ],
+        devtool: "source-map",
+      };
+    } else {
+      return {
+        plugins: [
+          new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+          }),
+        ],
+        devtool: "source-map",
+      };
+    }
+  },
+  integrity: true,
+  css: {
+    sourceMap: true,
   },
 };
