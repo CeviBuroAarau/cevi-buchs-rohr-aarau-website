376e69f0f52ec708ae58f73b7bc008d4102ade15
Patrick Uhlmann <git@uhlme.ch>
2021-03-08 23:59:58 +0100

    convert Aktivitäten page to typescript

---
+++ src/services/ActivitiesService.ts
@@ -0,0 +1,31 @@
+import { CockpitActivities, Activity, ActivityRaw } from "@/types/Activity";
+import { AxiosInstance, AxiosResponse } from "axios";
+
+export default class ActivitiesService {
+  private axios: AxiosInstance;
+
+  constructor(axios: AxiosInstance) {
+    this.axios = axios;
+  }
+
+  private async retrieveActivities(): Promise<Activity[]> {
+    const resp: AxiosResponse<CockpitActivities> = await this.axios.get<CockpitActivities>(
+      "collections/get/Activities"
+    );
+
+    const result: Activity[] = resp.data.entries.map((activity) => {
+      return {
+        title: activity.title,
+        url: process.env.VUE_APP_COCKPIT_FILES + activity.image.path,
+        thumb: process.env.VUE_APP_COCKPIT_FILES + activity.thumb.path,
+      };
+    });
+
+    return result;
+  }
+
+  async getActivities(): Promise<Activity[]> {
+    const activities: Activity[] = await this.retrieveActivities();
+    return activities;
+  }
+}
