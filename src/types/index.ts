d98c92dd9062c7944754551ae0d6b854830fe8d1
Patrick Uhlmann <git@uhlme.ch>
2021-03-16 20:49:22 +0100

    convert Leiterteam to typescript

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ export * from "./Activity";
 export * from "./Agenda";
 export * from "./Album";
 export * from "./Kontakt";
+export * from "./Leader";
 export * from "./Media";
 export * from "./Shop";
---
+++ tests/unit/services/LeaderService.spec.ts
@@ -0,0 +1,131 @@
+import { LeaderService } from "@/services";
+import { Leader } from "@/types";
+import axios from "axios";
+
+jest.mock("axios", () => {
+  return {
+    create: jest.fn(),
+    interceptors: {
+      request: { use: jest.fn(), eject: jest.fn() },
+      response: { use: jest.fn(), eject: jest.fn() },
+    },
+    get: () =>
+      Promise.resolve({
+        data: {
+          fields: {
+            scoutname: {
+              name: "scoutname",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            name: {
+              name: "name",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            group: {
+              name: "group",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            function: {
+              name: "function",
+              type: "multipleselect",
+              localize: false,
+              options: {
+                options:
+                  "Abteilungsleiter, Abteilungsleiterin, Gruppenleiter, Gruppenleiterin, Hilfsleiter, Hilfsleiterin, Materialverantwortlicher, L\\u00e4deliverantwortliche, Homepagebetreuer, Koordination YMCA, Lagerleiter",
+              },
+            },
+            birthyear: {
+              name: "birthyear",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            place: {
+              name: "place",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            profession: {
+              name: "profession",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            recreation: {
+              name: "recreation",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            inScountsSince: {
+              name: "inScountsSince",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            inScoutsBecause: {
+              name: "inScoutsBecause",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            bestExperiences: {
+              name: "bestExperiences",
+              type: "text",
+              localize: false,
+              options: [],
+            },
+            image: {
+              name: "image",
+              type: "image",
+              localize: false,
+              options: [],
+            },
+          },
+          entries: [
+            {
+              name: "Leah Dellenbach",
+              scoutname: "Luna",
+              function: ["Gruppenleiterin", "Abteilungsleiterin"],
+              group: "Spartacus",
+              birthyear: "1998",
+              place: "Aarau",
+              profession: "Neue Kantonsschule Aarau",
+              recreation: "Lesen, Jungschi, mit Freunden treffen",
+              inScountsSince: "ungef\\u00e4hr 2006",
+              inScoutsBecause:
+                "es lustig ist mit Gleichgesinnten etwas zu machen",
+              bestExperiences: "Pfila 2007, Leiteranl\\u00e4sse",
+              _mby: "5e8c4a1f30656581770002f3",
+              _by: "5e8c4a1f30656581770002f3",
+              _modified: 1612215935,
+              _created: 1586261102,
+              _id: "5e8c6c6e633238ddad0002c5",
+              image: {
+                path:
+                  "\\/storage\\/uploads\\/2021\\/02\\/01\\/2017-12-Luna.jpg_uid_6018765b6b961.webp",
+              },
+            },
+          ],
+          total: 1,
+        },
+      }),
+  };
+});
+
+const axiosInstance = axios as jest.Mocked<typeof axios>;
+
+describe("LeaderService", () => {
+  test("getLeaders", async () => {
+    const service: LeaderService = new LeaderService(axiosInstance);
+    const leaders: Leader[] = await service.getLeaders();
+    expect(leaders[0].place).toBe("Aarau");
+  });
+});
