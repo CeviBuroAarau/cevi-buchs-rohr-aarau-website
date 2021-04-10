b0ab5f5f8a6e221a2f54e891f8e7a6a294b02634
Patrick Uhlmann <git@uhlme.ch>
2021-04-10 16:00:57 +0200

    Test AxiosUtil

--- src/utils/AxiosUtil.ts
+++ src/utils/AxiosUtil.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { DateUtil } from "./DateUtil";
 
 export class AxiosUtil {
   static getCockpitInstance() {
@@ -10,5 +11,31 @@ export class AxiosUtil {
         "Content-Type": "application/json",
       },
     });
+  }
+
+  static dateConversionInterceptor(
+    originalResponse: AxiosResponse<any>,
+    fieldName: string
+  ) {
+    if (originalResponse.data.entries) {
+      // eslint-disable-next-line
+      originalResponse.data.entries.forEach((entry: any) =>
+        this.handleDates(entry, fieldName)
+      );
+    }
+    return originalResponse;
+  }
+
+  // eslint-disable-next-line
+  private static handleDates(body: any, searchKey: string) {
+    if (body === null || body === undefined || typeof body !== "object")
+      return body;
+
+    for (const key of Object.keys(body)) {
+      if (key == searchKey) {
+        const value = body[key];
+        body[key] = DateUtil.parseDateWithoutTime(value);
+      }
+    }
   }
 }
