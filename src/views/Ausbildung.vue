32f15b93585be9832f35b485069b2e595f373500
Patrick Uhlmann <git@uhlme.ch>
2021-05-03 20:11:09 +0200

    add GKu information

--- src/views/Ausbildung.vue
+++ src/views/Ausbildung.vue
@@ -17,8 +17,9 @@ export default class Ausbildung extends Vue {}
         </p>
         <ol>
           <li>
-            <a href="https://www.jugendundsport.ch/de/home.html">J+S</a>-Kurse:
-            Dabei handelt es sich um Kurse, welche von Jugend und Sport (dem
+            <a href="https://www.jugendundsport.ch/de/home.html">J+S</a>-Kurse
+            (z. B. Gruppenleiterkurs (GLK) oder Lagerleiterkurs (LLM)): Dabei
+            handelt es sich um Kurse, welche von Jugend und Sport (dem
             Sportförderungsprogramm des Bundesamt für Sport) anerkannt werden.
             Diese befähigen Dich eine Gruppe oder ein Lager zu leiten und haben
             eine offizielle Leiteranerkennung zur Folge. Der Cevi ist der
@@ -30,66 +31,48 @@ export default class Ausbildung extends Vue {}
             und Suchtprävention angeboten.
           </li>
           <li>
-            Diverse Kurse der Region: Diese bereiten Dich auf die J+S Kurse vor,
-            haben aber noch keine offizielle Leiteranerkennung zur Folge.
+            Diverse Kurse der Region (z. B. Grundlagenkurs): Diese bereiten Dich
+            auf die J+S Kurse vor, haben aber noch keine offizielle
+            Leiteranerkennung zur Folge.
           </li>
         </ol>
 
-        <a href="https://cevi-agsoluzg.ch/ausbildung/"
-          >Jährlich überarbeitetes Kursprogramm</a
-        >
+        <p>
+          Die ersten Kurse werden von der Region organisiert:
+          <a href="https://cevi-agsoluzg.ch/ausbildung/"
+            >Ausbildungsseite der Region</a
+          >. Fortgeschrittene Kurse teilweise über Cevi Schweiz:
+          <a href="https://www.cevi.ch/kurse/"
+            >Ausbildungsseite von Cevi Schweiz</a
+          >
+        </p>
 
         <h2 class="title is-2">Typische Laufbahn</h2>
         Hier siehst du die typische Laufbahn eines Cevileiters.
         <ul>
           <li>
-            Mit 14 Jahre kannst du den Grundlagenkurs absolvieren. Du wirst zum
+            <strong>14 Jahre</strong>: Grundlagenkurs (GKu). Du wirst zum
             Hilfsleiter ausgebildet und kannst fortan bei einer Gruppe als
             Leiter Teilaufgaben übernehmen. Die Verantwortung liegt aber
-            weiterhin beim Gruppenleiter. Es findet jeweils im Frühling und im
-            Herbst ein Kurs statt.
+            weiterhin beim Gruppenleiter.
           </li>
           <li>
-            Mit 15 Jahren kannst du das Zwischenmodul 1 absolvieren. Dort
-            erarbeitest du selbstständig erste Programme und lernst die
-            Grundlagen der ersten Hilfe.
+            <strong>15 Jahre</strong>: Zwischenmodul 1 (ZM 1). Dort erarbeitest
+            du selbstständig erste Programme und lernst die Grundlagen der
+            ersten Hilfe.
           </li>
           <li>
-            Mit 16 Jahren kannst du das Zwischenmodul 2 absolvieren. Es bringt
-            Dich in den Themen Pionier, Orientieren sowie Psychologie/Gruppe
+            <strong>16 Jahre</strong>: Zwischenmodul 2 (ZM 2). Es bringt Dich in
+            den Themen Pioniertechnik, Orientieren sowie Psychologie/Gruppe
             weiter.
           </li>
           <li>
-            Mit 18 Jahren kannst du den Gruppenleiterkurs absoliveren. Dieser
-            befähigt Dich eine eigene Gruppe zu führen.
-          </li>
-          <li>
-            Mit 19 Jahren kannst du den Lagerleiterkurs absoliveren. Dieser
-            befähigt Dich ein Lager unter J+S anzumelden und durchzuführen.
+            <strong>18 Jahre</strong>: Gruppenleiterkurs (GLK). Dieser befähigt
+            Dich eine eigene Gruppe zu führen. Es ist der erste J+S Kurs.
           </li>
         </ul>
 
-        <h2 class="title is-2">Auffrischung J+S Leiter</h2>
-
-        Sobald du einen Gruppen- oder Lagerleiterkurs absolivert hast wirst du
-        in der Datenbank des Bundesamt für Sport registriert. Damit du Deine
-        Anerkennung behälst musst du regelmässig (alle zwei Jahre)
-        Auffrischungskurse besuchen. Du kannst dabei aus verschiedenen Angeboten
-        auswählen:
-
-        <ul>
-          <li>Rüeblichrut Kurs</li>
-          <li>
-            J+S Wahlmodule zu verschiedenen Themen: Winter, Berg, Kinder- und
-            Jugendsport, Waser. Diese sind eher "Fun" orientiert
-          </li>
-          <li>
-            J+S Sicherheitsmodule zu verschiedenen Themen: Winter, Wasser und
-            Berg. Diese geben Dir weitere Zulassungen (z. B. Schneetouren).
-          </li>
-        </ul>
-
-        <h2 class="title is-2">Weitere Kurse</h2>
+        Je nach Interesse kannst du auch weitere Kurse absolvieren:
         <ul>
           <li>
             Ab 14 Jahre kannst du den Frösch-Up besuchen. Dieser Kurs
@@ -102,8 +85,71 @@ export default class Ausbildung extends Vue {}
             Abteilung vermittelt.
           </li>
           <li>
-            Verschiedene "Kaderkurse": J+S Expertenkurs, J+S Coachkurs und J+S
-            Kursleiterkurs
+            Mit 19 Jahren kannst du den Lagerleiterkurs absolvieren. Dieser
+            befähigt Dich ein Lager unter J+S anzumelden und durchzuführen.
+          </li>
+          <li>
+            Später: Verschiedene "Kaderkurse" beispielsweise J+S Expertenkurs,
+            J+S Coachkurs und J+S Kursleiterkurs.
+          </li>
+        </ul>
+
+        <h2 class="title is-2">Grundlagenkurs (GKu)</h2>
+
+        <strong>Was ist der Grundlagenkurs?</strong>
+        <p>
+          Der Grundlagenkurs findet jeweils zweimal im Jahr statt (Frühling und
+          Herbst). Er besteht jeweils aus einem Vorweekend (zur Einstimmung in
+          den Kurs) und der effektiven Kurswoche. Der Teilnehmerbeitrag beträgt
+          in der Regel 300 CHF. Unser Verein verfügt über einen Ausbildungsfonds
+          über den wir den Teilnehmerbeitrag auf Antrag zurückerstatten damit
+          dieser keine Hürde darstellt.
+        </p>
+
+        <strong
+          >Mit welchem Alter sollte ich den Grundlagenkurs absolvieren?</strong
+        >
+        <p>
+          Das offizielle Mindestalter ist 14. Es wird dafür jeweils auf den
+          Jahrgang geschaut (Jahr in dem man 14 wird) und nicht auf das
+          Geburtsdatum. In der Regel ist es auf eigenen Wunsch auch möglich den
+          Kurs bereits ein Jahr früher zu absolvieren jedoch unter dem Vorbehalt
+          das es nicht zuviele reguläre Anmeldungen hat. Fühlt man sich noch
+          nicht bereit kann der Kurs auch ein Jahr später noch absolviert
+          werden.
+        </p>
+
+        <strong>Wie melde ich mich an?</strong>
+        <p>
+          Die Kurse werden jeweils nicht von den Abteilungen, sondern von der
+          Region durchgeführt. Diese hat eine eigene
+          <a href="https://cevi-agsoluzg.ch/ausbildung/">Ausbildungsseite</a>.
+          Auf dieser findest du eine Liste der Kurse. Wenn du einen Kurs
+          auswählst findest du den Anmeldetalon. Sobald du diesen ausgefüllt und
+          eingesendet hast bist du dabei. Hinweis: da der Grundlagenkurs noch
+          kein J+S Kurs ist kannst du alle Felder betreffend J+S Coach leer
+          lassen.
+        </p>
+
+        <h2 class="title is-2">Auffrischung J+S Leiter</h2>
+
+        <p>
+          Sobald du einen Gruppen- oder Lagerleiterkurs absolviert hast wirst du
+          in der Datenbank des Bundesamt für Sport registriert. Damit du Deine
+          Anerkennung behälst musst du regelmässig (alle zwei Jahre)
+          Auffrischungskurse besuchen. Du kannst dabei aus verschiedenen
+          Angeboten auswählen:
+        </p>
+
+        <ul>
+          <li>Rüeblichrut Kurs</li>
+          <li>
+            J+S Wahlmodule zu verschiedenen Themen: Winter, Berg, Kinder- und
+            Jugendsport, Waser. Diese sind eher "Fun" orientiert
+          </li>
+          <li>
+            J+S Sicherheitsmodule zu verschiedenen Themen: Winter, Wasser und
+            Berg. Diese geben Dir weitere Zulassungen (z. B. Schneetouren).
           </li>
         </ul>
       </div>
