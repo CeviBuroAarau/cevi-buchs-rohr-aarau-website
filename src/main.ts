1d62bb967595b3e57a123b1009935d959a99e67f
Patrick Uhlmann <github@uhlme.ch>
2024-11-09 23:16:14 +0100

    remove matomo
    replaced by Shynet

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import { ErrorReportingService } from "@/services";
-import VueMatomo from "vue-matomo";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { formatDateFilter } from "@/filters";
@@ -22,26 +21,6 @@ require("./assets/main.scss");
 
 library.add(faPhone, faEnvelope, faUser, faBars);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-
-if (process.env.NODE_ENV === "production") {
-  Vue.use(VueMatomo, {
-    host: process.env.VUE_APP_MATOMO_HOST,
-    siteId: process.env.VUE_APP_MATOMO_SITE_ID,
-    trackerFileName: "matomo",
-    router: router,
-    enableLinkTracking: true,
-    requireConsent: false,
-    trackInitialView: true,
-    disableCookies: false,
-    enableHeartBeatTimer: true,
-    heartBeatTimerInterval: 15,
-    debug: false,
-    userId: undefined,
-    cookieDomain: undefined,
-    domains: undefined,
-    preInitActions: [],
-  });
-}
 
 new Vue({
   router,
